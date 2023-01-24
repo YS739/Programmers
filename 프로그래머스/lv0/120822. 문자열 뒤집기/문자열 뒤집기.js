2aff2879dec578b5e267ce7aa61971779e2fe2f1
Yunsuk <suk921@gmail.com>
2023-01-24 20:19:38 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    let a = []
+    for(let i=1; i<=my_string.length; i++){
+    let reverseA = (my_string[my_string.length-i])
+        a.push(reverseA)
+    }          
+return a.join("")
+}
