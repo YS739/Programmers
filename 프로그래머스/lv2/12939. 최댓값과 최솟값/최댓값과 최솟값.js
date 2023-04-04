c9fa102c3b228102531692b9733351ec29f519c5
Yunsuk <suk921@gmail.com>
2023-04-04 18:47:54 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,6 @@
 const solution = (s) => {
   let answer = "";
   const test = s.split(" ").sort((a, b) => a - b);
-  console.log(test);
   answer = test[0] + " " + test[test.length - 1];
   return answer;
 };
