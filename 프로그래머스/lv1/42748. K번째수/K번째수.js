5b52bcbe29fcccc0d04ef70314e7e02e61cb17c0
Yunsuk <suk921@gmail.com>
2023-04-05 22:36:31 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,11 @@
+const solution = (array, commands) => {
+  let answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    newArray = array
+      .slice(commands[i][0] - 1, commands[i][1])
+      .sort((a, b) => a - b);
+    answer.push(newArray[commands[i][2] - 1]);
+  }
+
+  return answer;
+};
