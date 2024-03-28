d8bd1994c0c51f697afbc0434b86960515728c43
Yunsuk <suk921@gmail.com>
2024-03-28 13:31:53 +0900

    [Bronze V] Title: A/B, Time: 144 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,14 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+readline
+  .on("line", function (line) {
+    input = line.split(" ").map((el) => parseInt(el));
+  })
+  .on("close", function () {
+    const [A,B] = input;
+    console.log(A/B);
+    process.exit();
+  });
